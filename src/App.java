59424461fc50e5a226c0ea054177cee351d2e57e
USERNAME <stella.p2002@interia.pl>
2021-02-17 16:28:14 +0100

    vb

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+      
         /* 
         to jest wpisywanie imienia i wyswietlanie go
         System.out.println("Podaj swoje imię");
@@ -11,7 +11,6 @@ public class App {
          System.out.println("Twoje imię to "+firstName);
          scanner.close();
         */ 
-        
       /* 
        kalkulator 2 liczb
        System.out.println("podaj pierwsza liczbę");
@@ -21,7 +20,6 @@ public class App {
         System.out.println(a+" + "+b+" = "+(a+b));
         scanner.close();
         */
-
      /*  int nr = 10;
        
        if(nr == 2){
@@ -32,9 +30,6 @@ public class App {
         System.out.println("Jakas inna liczba niz 3 i 2 ");
        }
 */
-
-
-
 /*System.out.println("Wpisz pierwsza liczbe");
 int a = scanner.nextInt();
 
@@ -56,23 +51,59 @@ if(o.equals("+") ){
     System.out.println("zly operator");
 }
 */
-
-String o = "+"
+/*
+String o = "*";
 switch(o){
+    case "+":
     System.out.println("PLUS");
     break;
-    case "=";
+    case "-":
     System.out.println("MINUS");
     break;
-    case "*";
+    case "*":
     System.out.println("RAZY");
     break;
-    default
+    default:
     System.out.println("CO INNEGO");
+}*/
 
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Podaj pierwsza liczbe");
+int a = scanner.nextInt();
+
+System.out.println("podaj operator + , - , * , /, %");
+scanner.nextLine();
+String o = scanner.nextLine();
+
+System.out.println("Podaj druga liczbe");
+int b = scanner.nextInt();
+
+switch (o) {
+   case "+":
+   System.out.println(a+" + "+b+" = "+(a+b));
+   break;
+
+   case "-":
+   System.out.println(a+" - "+b+" = "+(a-b));
+   break;
+
+   case "*":
+   System.out.println(a+" * "+b+" = "+(a*b));
+   break;
+
+   case "/":
+   System.out.println(a+" / "+b+" = "+(a/b));
+   break;
+
+   case "%":
+   System.out.println(a+" % "+b+" = "+(a%b));
+   break;
+
+   default:
+   System.out.println(" nie ma takiego operatora");
+   break;
 }
-
-
 
 
         scanner.close();
